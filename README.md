905d2bf61e2b6e2e9e83048801add14c1e46e7e6
杨衡 <yangheng@goumin.com>
2018-08-02 00:55:14 +0800

    项目初始化提交

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # MybatisGenerator
 Mybatis的逆向工程
+
+只需要修改数据库连接 表名,运行one.start.Generator.main 即可生成 mapper.xml mapper的接口和pojo类
+根据项目需求 自行粘贴复制到项目中 (记得修改package包名)
+
+## maven下才可运行
---
+++ one/mapper/TbUserMapper.java
@@ -0,0 +1,16 @@
+package one.mapper;
+
+import java.util.List;
+import one.domain.TbUser;
+
+public interface TbUserMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(TbUser record);
+
+    TbUser selectByPrimaryKey(Long id);
+
+    List<TbUser> selectAll();
+
+    int updateByPrimaryKey(TbUser record);
+}
